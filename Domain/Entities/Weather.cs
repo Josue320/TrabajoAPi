cf74c0ff42455a63dbe3598e817eee0c20745c43
josue omar flores diaz <josue.omar68u@std.uni.edu.ni>
2022-04-04 19:09:32 -0600

    Entities

---
+++ Domain/Entities/Weather.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities
+{
+    public class Weather
+    {
+        public string main { get; set; }
+        public string description { get; set; }
+        public string icon { get; set; }
+    }
+}
